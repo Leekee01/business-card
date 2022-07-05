3922a8254dc7a943b34de242ec96d175dda4aa76
Leekee01 <zeje23@naver.com>
2022-07-05 16:10:37 +0900

    Business card
    Business card portfolio

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { memo } from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import "./index.module.css";
+import App from "./app";
+import AuthService from "./service/auth_service";
+import ImageUploader from "./service/image_uploader";
+import ImageFileInput from "./components/image_file_input/image_file_input";
+import CardRepository from "./service/card_repository";
+import { firebaseApp } from "./service/firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const authService = new AuthService(firebaseApp);
+const cardRepository = new CardRepository(firebaseApp);
+const imageUploader = new ImageUploader();
+const FileInput = memo((props) => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+));
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      authService={authService}
+      FileInput={FileInput}
+      cardRepository={cardRepository}
+    />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
