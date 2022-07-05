5dad514683112c795a143d4ee7edb89a85bef94b
Leekee01 <zeje23@naver.com>
2022-07-05 17:21:17 +0900

    business card
    business card portfolio currection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { memo } from 'react';
-import ReactDOM from 'react-dom';
-import './index.module.css';
-import App from './app';
-import AuthService from './service/auth_service';
-import ImageUploader from './service/image_uploader';
-import ImageFileInput from './components/image_file_input/image_file_input';
-import CardRepository from './service/card_repository';
-import { firebaseApp } from './service/firebase';
+import React, { memo } from "react";
+import ReactDOM from "react-dom";
+import "./index.module.css";
+import App from "./app";
+import AuthService from "./service/auth_service";
+import ImageUploader from "./service/image_uploader";
+import ImageFileInput from "./components/image_file_input/image_file_input";
+import CardRepository from "./service/card_repository";
+import { firebaseApp } from "./service/firebase";
 
 const authService = new AuthService(firebaseApp);
 const cardRepository = new CardRepository(firebaseApp);
@@ -16,12 +16,10 @@ const FileInput = memo((props) => (
 ));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App
-      authService={authService}
-      FileInput={FileInput}
-      cardRepository={cardRepository}
-    />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App
+    authService={authService}
+    FileInput={FileInput}
+    cardRepository={cardRepository}
+  />,
+  document.getElementById("root")
 );
